40ba624ff616ff216421357776e3ee7033cb9d3c
Riyash MH <riyash@gmail.com>
2023-11-28 16:11:36 +0530

    Fixing issues 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
-document.getElementById('menuToggle').addEventListener('click', function() {
-    var navItems = document.getElementById('navbar').querySelector('ul');
-    navItems.style.display = navItems.style.display === 'block'? 'none' : 'block';
+document.getElementById('menuToggle').addEventListener('click', function(event) {
+    var mobileMenu = document.getElementById('mobileMenu');
+    console.log("got event");
+    event.stopPropagation();
+    console.log("stopped prop");
+    mobileMenu.classList.toggle('mobileMenuVisible');
+    //mobileMenu.style.display = (mobileMenu.stylwhitee.display === 'block' ? 'none' : 'block');
 }
 );
+
+window.addEventListener('click', function(event) {
+    var mobileMenu = document.getElementById('mobileMenu');
+    console.log("got window event");
+    if (mobileMenu.classList.contains('mobileMenuVisible') && event.target !== mobileMenu && !mobileMenu.contains(event.target)) {
+        console.log("removing settings");
+        mobileMenu.classList.remove('mobileMenuVisible');
+    }
+});
